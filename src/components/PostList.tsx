77acd628f1b971b0b641ea42e7331716ffa40cdf
kangmuni <kangmoonee@gmail.com>
2024-02-14 12:18:09 +0900

    포스트 목록 서비스,이미지URL 구현

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,4 +1,12 @@
+'use client';
+import { SimplePost } from '@/model/post';
+import useSWR from 'swr';
+
 export default function PostList() {
+  const { data, isLoading } = useSWR<SimplePost[]>('/api/post');
+
+  console.log(data);
+
   return (
     <section>
       <p>PostList</p>
